b26e902fd1d471817c65bb18e22caa8be78e6706
Mateusz Lenik <mlen@mlen.pl>
2018-09-10 23:39:53 +0200

    Implement config file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #[macro_use]
 extern crate serde_derive;
+
+#[macro_use]
+extern crate clap;
+
 extern crate toml;
 extern crate ddc_hi;
 
@@ -7,6 +11,7 @@ use std::{thread, time, io, process, collections::HashMap};
 use ddc_hi::Ddc;
 use std::io::Read;
 use std::iter::Iterator;
+use clap::{Arg, App};
 
 #[derive(Deserialize)]
 struct Config {
@@ -16,6 +21,13 @@ struct Config {
 impl Config {
     fn get(&self, info: &ddc_hi::DisplayInfo) -> Option<&Display> {
         self.displays.iter().filter(|d| d.matches(info)).next()
+    }
+
+    fn parse(path: &str) -> Config {
+        let mut data = String::new();
+        let mut file = std::fs::File::open(path).expect("config file not found");
+        file.read_to_string(&mut data).expect("failed to read configuration");
+        toml::from_str(data.as_str()).expect("failed to parse configuration")
     }
 }
 
@@ -52,16 +64,28 @@ impl Action {
     }
 }
 
-fn poll(config: &Config, displays: &mut Vec<ddc_hi::Display>, prev_state: &mut HashMap<String, u16>) {
-    for mut display in displays {
-        if display.update_capabilities().is_ok() {
-            if let Some(d) = config.get(&display.info) {
-                if let Ok(value) = display.handle.get_vcp_feature(d.feature) {
-                    let current = value.value();
-                    let old = *prev_state.entry(d.serial.clone()).or_insert(current);
-                    if current != old {
-                        prev_state.insert(d.serial.clone(), current);
-                        d.get(current).and_then(|a| a.run().ok());
+struct Poller {
+    config: Config,
+    displays: Vec<ddc_hi::Display>,
+    state: HashMap<String, u16>
+}
+
+impl Poller {
+    fn new(config: Config) -> Poller {
+        Poller { config, displays: ddc_hi::Display::enumerate(), state: HashMap::new() }
+    }
+
+    fn poll(&mut self) {
+        for mut display in self.displays.iter_mut() {
+            if display.update_capabilities().is_ok() {
+                if let Some(d) = self.config.get(&display.info) {
+                    if let Ok(value) = display.handle.get_vcp_feature(d.feature) {
+                        let current = value.value();
+                        let old = *self.state.entry(d.serial.clone()).or_insert(current);
+                        if current != old {
+                            self.state.insert(d.serial.clone(), current);
+                            d.get(current).and_then(|a| a.run().ok());
+                        }
                     }
                 }
             }
@@ -70,15 +94,21 @@ fn poll(config: &Config, displays: &mut Vec<ddc_hi::Display>, prev_state: &mut H
 }
 
 fn main() {
-    let mut data = String::new();
-    let mut file = std::fs::File::open("config.toml").expect("config.toml not found");
-    file.read_to_string(&mut data).expect("failed to read configuration");
-    let config: Config = toml::from_str(data.as_str()).expect("failed to parse configuration");
+    let matches = App::new(crate_name!())
+        .about(crate_description!())
+        .version(crate_version!())
+        .author(crate_authors!(", "))
+        .arg( Arg::with_name("config")
+            .short("f")
+            .value_name("FILE")
+            .takes_value(true)
+            .help("Path to the configuration file")).get_matches();
 
-    let mut prev_state = HashMap::new();
-    let mut displays = ddc_hi::Display::enumerate();
+    let path = matches.value_of("config").unwrap_or("config.toml");
+    let config = Config::parse(path);
+    let mut poller = Poller::new(config);
     loop {
-        poll(&config, &mut displays, &mut prev_state);
+        poller.poll();
         thread::sleep(time::Duration::from_millis(1000));
     }
 }
